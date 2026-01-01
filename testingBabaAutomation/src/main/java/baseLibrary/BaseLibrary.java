ccbafceaab4a7a5c314a48261e63194f55411b47
Sakshi <Sakshisuryavanshi904@gmail.com>
2026-01-01 20:25:02 +0530

    Add files via upload

---
+++ testingBabaAutomation/src/main/java/baseLibrary/BaseLibrary.java
@@ -0,0 +1,17 @@
+package baseLibrary;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class BaseLibrary {
+	
+	public static WebDriver driver ;
+	
+	@Test
+	public void launchUrl()
+	{
+		 driver = new ChromeDriver();
+	}
+
+}
